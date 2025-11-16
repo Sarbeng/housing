d621e9bade1e3b55bcb80718b4f8734cdcfd7a11
Sarbeng <ksarbengbaafi@gmail.com>
2025-11-16 17:35:40 +0000

    registration section removed as it is not required

--- database/migrations/2025_11_15_140852_create_tenant_fee_allocations.php
+++ database/migrations/2025_11_15_140852_create_tenant_fee_allocations.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('tenant_fee_allocations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('tenant_id')->constrained('tenants')->onDelete('cascade');
-            $table->foreignId('tenant_id')->constrained();
             $table->foreignId('service_payment_id')->constrained('service_payments')->onDelete('cascade');
             $table->decimal('allocated_amount', 8, 2); // Tenant's share of the fee
             $table->decimal('amount_paid_by_tenant', 8, 2)->default(0.00);
